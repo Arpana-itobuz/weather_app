4555ebe059561975845b21c6d553b9d9ed57d581
Arpana <arpana@itobuz.com>
2023-03-02 18:06:45 +0530

    version 3

--- js/script.js
+++ js/script.js
@@ -1,19 +1,19 @@
+const temperature = document.getElementById("temperature");
+const feelsLike = document.getElementById("feels-like");
 const locationPlace = document.querySelector("input");
 let image = document.querySelector(".top-section img");
 console.log(image);
+
 async function getWeatherData(query) {
   const response = await fetch(
     `http://api.weatherapi.com/v1/current.json?key=0c80b2b56f1943ada19100744230103&q=${query}&aqi=no`
   ).then(function (res) {
     return res.json();
   });
-  console.log(response);
-  locationPlace.value = response.location.name;
-  // console.log(locationPlace.innerHTML);
 
-  const temperature = document.getElementById("temperature");
+  locationPlace.value = response.location.name;
   temperature.innerHTML = `${response.current.temp_c}°`;
-  console.log(response.current.temp_c);
+ 
   if (response.current.temp_c < 5) {
     image.src = "./images/snowy-6.svg";
   } else if (response.current.temp_c >= 5 && response.current.temp_c < 10) {
@@ -26,7 +26,6 @@ async function getWeatherData(query) {
     image.src = "./images/cloudy-day-1.svg";
   }
 
-  const feelsLike = document.getElementById("feels-like");
   feelsLike.innerHTML = `Feels ${response.current.feelslike_c}°`;
 }
 
@@ -37,6 +36,6 @@ locationPlace.addEventListener("keyup", (e) => {
   }
 });
 
-window.onload = () => {
-  getWeatherData("kolkata");
-};
+(()=>{
+  getWeatherData("kolkata")
+})();
